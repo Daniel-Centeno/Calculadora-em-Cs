a472d81d1599f5f71b949fca95802c1fc8e3bf37
Dandan <97252704+Daniel-Centeno@users.noreply.github.com>
2023-10-31 19:20:37 -0300

    correcao de bug na execucao,  adicionados comandos ext e default

--- code/Calculadora/Program.cs
+++ code/Calculadora/Program.cs
@@ -9,9 +9,10 @@ namespace Calculadora
             // variável que diz se o programa deve ou não ficar ativo
             bool running = true;
 
-            if (running == true)
+            while (running == true)
             {
-                //Mensagem de boas vindas
+                Console.Clear();
+                //Mensagem de boas vindas / lista de comandos
                 Console.WriteLine("Ola! digite a operação que deseja fazer");
                 Console.WriteLine("add - soma");
                 Console.WriteLine("sub - para subtrair");
@@ -20,7 +21,24 @@ namespace Calculadora
                 Console.WriteLine("rai - tira  a raiz do número");
                 Console.WriteLine("ext - sair do programa");
 
+                //Guarda a resposta numa string
                 string answer = Console.ReadLine();
+
+                switch (answer)
+                {
+                    case "ext":
+                        //Sai do programa
+                        running = false;
+                        Console.Clear();
+                        Console.WriteLine("Muito obrigado pela preferência, volte sempre!");
+                        break;
+                    default:
+                        Console.Clear();
+                        Console.WriteLine("Desculpe, não entendi muito bem, talvez tenha havido um erro de digitação");
+                        Console.WriteLine("Aperte Enter para continuar");
+                        Console.ReadLine();
+                        break;
+                }
             }
         }
     }
