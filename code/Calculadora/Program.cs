3e34144adc649b1d99d67e74ed9dc1060b8c8136
Dandan <97252704+Daniel-Centeno@users.noreply.github.com>
2023-10-31 19:06:29 -0300

    Lista de comandos

--- code/Calculadora/Program.cs
+++ code/Calculadora/Program.cs
@@ -6,7 +6,22 @@ namespace Calculadora
     {
         public static void Main(string[] args)
         {
+            // variável que diz se o programa deve ou não ficar ativo
+            bool running = true;
 
+            if (running == true)
+            {
+                //Mensagem de boas vindas
+                Console.WriteLine("Ola! digite a operação que deseja fazer");
+                Console.WriteLine("add - soma");
+                Console.WriteLine("sub - para subtrair");
+                Console.WriteLine("mul - multiplicar");
+                Console.WriteLine("div - dividir");
+                Console.WriteLine("rai - tira  a raiz do número");
+                Console.WriteLine("ext - sair do programa");
+
+                string answer = Console.ReadLine();
+            }
         }
     }
 }
